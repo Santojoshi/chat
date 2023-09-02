8a7f77c58d1da79293c3dfdbb5dda1eaa4cbf2e2
santo <joshi.santosh7878@gmail.com>
2023-09-02 12:51:04 +0530

    change

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const cors = require('cors');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server,{
+  cors: {origin: "*"
+}
+});
+const users={};
+// Enable CORS for the specific orig
+app.use(cors());
+
+app.get('/', (req,res)=>{
+  res.send("Hii working chat server")
+})
+// Socket.IO connection handling
+io.on('connection', (socket) => {
+  socket.on('new-user-joined', (name) => {
+    console.log('user', name);
+    users[socket.id] = name;
+    socket.broadcast.emit('user-joined', name);
+  });
+
+  socket.on('send', (message) => {
+    socket.broadcast.emit('receive', { message: message, name: users[socket.id] });
+  });
+});
+
+const PORT = 1000 || process.env.PORT;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
